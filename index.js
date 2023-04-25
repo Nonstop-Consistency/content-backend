b8dd4a294e75dbfaeb42beb2726b4d0e4e1729c5
Iman Abdurrahman <72773420+kentangtelo@users.noreply.github.com>
2023-04-25 07:18:20 +0700

    testing

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Tugas App Engine");
+  res.json({ success: true, message: "success" });
 });
 
 app.get("/content", async (req, res) => {
